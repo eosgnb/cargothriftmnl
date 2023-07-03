e926fc5ea2b8e7e27ccc74de100d7c19598e4af8
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-07-04 01:44:39 +0800

    New Homepage

--- homepage.php
+++ homepage.php
@@ -8,44 +8,65 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,800" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php include 'navigation.php';?>
     
             <div class="home">
-                <h1 style="font-size:60px;">Your Destination for Quality Pre-Loved Treasures.</h1><br>
-                <h2 style="font-size: 60px;">Fashion you'll love.</h2><br><br><br>
-                <p>"For a one-of-a-kind shopping experience, check out CargoThrift.mnl. <br>
-                    Since 2021, we’ve been known for our great service and impeccable style. <br> It's our pleasure to help you find what you're looking for. <br> If you have any questions, just reach out.</p>
-                    <button class="button-73" role="button">Learn More</button>
+                <h1>Your Destination for Quality <br> Pre-Loved Treasures.</h1><br>
+                <h2>Since 2021, CARGOTHRIFT.MNL been known for our <br> great service and impeccable style.</h2><br><br><br>
+                    <button class="button-73" role="button">Shop Now</button>
             </div> 
-    <div class="container1">
-        <div class="slider">
-            <img src="Images/4.png"  alt="slider" />
+      
+        <div class="main-container1"> 
+            <div id="images">
+                <img src="Images/pants1.jpg" alt=”Photo” width=350 height=350>
+                <button class="pants1">CARGO PANTS</button>
+            </div>
+            <div id="images">
+                <img src="Images/pants2.jpg" alt=”Photo” width=350 height=350>
+                <button class="pants2">CORDUROY</button>
+            </div>
+            <div id="images">
+                <img src="Images/pants3.jpg" alt=”Photo” width=350 height=350>
+                <button class="pants3">VINTAGE</button>
+            </div>
         </div>
-            <div class="text">
-                <h1>CargoThrift.Mnl.</h1>
-                <h3>Founded in 2023, CargoThrift.mnl was born
-                    from a vision to revolutionize fashion<br> and
-                    promote sustainability. We curate pre-loved
-                    clothing, making fashion affordable <br> and 
-                    environmentally friendly. With a commitment 
-                    to reducing waste and empowering conscious
-                    consumerism, we provide a platform where
-                    style meets sustainability.</h3>
-                    <h3>CargoThrift Shop is a unique and innovative retail destination <br> 
-                    that brings together the thrill of treasure hunting with the convenience of online shopping. <br> 
-                    We specialize in offering a wide variety of pre-loved and gently used items, carefully curated to <br> 
-                    cater to the diverse tastes and preferences of our customers.</h3>
-                    <button class="arrow-button">About Us<span class="arrow"></span></button>
-            </div> 
-    </div>
+            <div class="main-container">        
+                <div class="container1">
+                    <div class="slider">
+                        <img src="Images/CARGOTHRIFT.png"  alt="slider" />
+                    </div>   
+                </div>
+                <div class="container2">
+                    <div class="text">
+                        <h1>About Us</h1>
+                        <h3>Founded in 2023, CargoThrift.mnl was born
+                        from a vision to <br> revolutionize fashion and
+                        promote sustainability. We curate <br>pre-loved
+                        clothing, making fashion affordable and 
+                        environmentally <br>friendly. With a commitment 
+                        to reducing waste and empowering <br>conscious
+                        consumerism, we provide a platform where
+                        style meets <br>sustainability.</h3>
+                        <h3>CargoThrift Shop is a unique and innovative retail destination
+                        that <br> brings together the thrill of treasure hunting with the convenience <br> of online shopping.
+                        We specialize in offering a wide variety of <br> pre-loved and gently used items, carefully curated to 
+                        cater <br>to the diverse tastes and preferences of our customers.</h3>
+                    </div> 
+                </div>
+            </div>
+                <div class="copyright">
+                    <center>
+                        <span>Copyright © 2023, CARGOTHRIFT.MNL</span>
+                    </center>
+                </div>
         
 
-
-    <?php include 'footer.php';?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- navigation.php
+++ navigation.php
@@ -1,6 +1,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <div class="main-navbar shadow-sm sticky-top">
@@ -8,39 +11,44 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-1 my-auto d-none d-sm-none d-md-block d-lg-block">
-                        <img src="Images/1.png" height="auto" width="50%">
+                        <h8 style="color:white; font-family:Josefin Sans;"> CARGOTHRIFT.MNL</h8>
                     </div>
                     <div class="col-md-8 my-auto">
-                      <ul class="nav justify-content-start">
+                      <ul class="nav justify-content-center p-0 m-1">
                         <li class="nav-item">
-                            <a class="nav-link" href="homepage.php"style="color: white">Home</a>
+                            <a class="nav-link" href="homepage.php"style="color: white; font-family: Lato;">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="newarrivals.php" style="color: white">New Arrivals</a>
+                            <a class="nav-link" href="#" style="color: white; font-family: Lato;">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: white">About</a>
+                            <a class="nav-link" href="#" style="color: white; font-family: Lato;">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: white">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: #dff0b9">Shop</a>
+                            <a class="nav-link" href="#" style="color: white; font-family: Lato; ">Shop</a>    
                         </li>
                     </ul> 
                     </div>
                     <div class="col-md-3 my-auto">
                         <ul class="nav justify-content-end">
+
+                        <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: white; font-family: Lato;" >
+                                    <i class="fa fa-user"></i> Profile
+                                </a>
+                            </li>
                             
                             <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: white" >
+                                <a class="nav-link" href="#" style="color: white; font-family: Lato;" >
                                     <i class="fa fa-shopping-cart"></i> Cart (0)
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: white" >
+                                <a class="nav-link" href="#" style="color: white; font-family: Lato;" >
                                     <i class="fa fa-heart"></i> Wishlist (0)
                                 </a>
+                            </li>
+                           
                         </ul>
                     </div>
                 </div>
