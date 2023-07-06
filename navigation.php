5782eda74d8cd383ef6c2e5008f998700dbb99ee
eosgnb <eosuganob@iskolarngbayan.pup.edu.ph>
2023-07-06 22:18:28 +0800

    change navbar

--- navigation.php
+++ navigation.php
@@ -1,12 +1,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat&family=Open+Sans&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <!-- Local CSS -->
     <link rel="stylesheet" href="style.css">
 </head>
-<div class="main-navbar shadow-sm sticky-top">
+<!-- <div class="main-navbar shadow-sm sticky-top">
         <div class="top-navbar">
             <div class="container-fluid">
                 <div class="row">
@@ -41,4 +44,36 @@
                 </div>
             </div>
         </div>
+    </div> -->
+
+  <nav class="navbar navbar-expand-lg sticky-top px-3">
+    <div class="container-fluid">
+        <a class="navbar-brand mr-5" href="#">CARGOTHRIFT.MNL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-shopping-cart"></i> Cart
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
+</nav>
